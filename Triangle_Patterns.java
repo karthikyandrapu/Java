4506b5e804b64bfb72062afad744887813806cd7
Karthik Yandrapu <durgakarthikyandrapu@gmail.com>
2023-03-06 01:18:00 +0530

    Create Triangle_Patterns.java

---
+++ Triangle_Patterns.java
@@ -0,0 +1,13 @@
+public class Triangle_Patterns {
+ public static void main(String args[]) {
+	 int i,j,n=5;
+	 for(i=1;i<=n;i++)
+		 {
+		 for(j=1;j<=i;j++)
+		 {
+			 System.out.print("* ");
+		 }
+		 System.out.print("\n");
+		 }
+ }
+}
