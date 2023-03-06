afab6c55f191736a103da8a42af25d04b8bb24cc
Karthik Yandrapu <durgakarthikyandrapu@gmail.com>
2023-03-06 11:39:03 +0530

    Create Declare_initialisation.java

---
+++ Declare_initialisation.java
@@ -0,0 +1,15 @@
+class Declare_initialisation {
+public static void main(String args[])
+{
+int a[]=new int[5],i;
+a[0]=1;
+a[1]=2;
+a[2]=3;
+a[3]=4;
+a[4]=5;
+for(i=0;i<a.length; i++)
+{
+System.out.print(a[i]+" ");
+}
+}
+}
