cf81e73b2d5a0796b4a05521db7274120001f9d7
Karthik Yandrapu <durgakarthikyandrapu@gmail.com>
2023-03-11 00:05:59 +0530

    Create Default_values_of_primitive_Data_types.java

---
+++ Default_values_of_primitive_Data_types.java
@@ -0,0 +1,22 @@
+public class Default_values_of_primitive_Data_types {
+	static int a;
+	static float b;
+	static short c;
+	static long d;
+	static double e;
+	static byte f;
+	static boolean g;
+	static char h;
+	public static void main( String[] args)
+	{
+		System.out.println("Default Values of Primitive Data Types:");
+		System.out.println("Int:"+a);
+		System.out.println("Float:"+b);
+		System.out.println("Short:"+c);
+		System.out.println("Long:"+d);
+		System.out.println("Double:"+e);
+		System.out.println("Byte:"+f);
+		System.out.println("Boolean:"+g);
+		System.out.println("Char:"+h);
+		}
+}
